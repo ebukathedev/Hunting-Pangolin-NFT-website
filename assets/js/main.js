b1e3334cf63ff88ce224f0d39b8627e1c7e44aef
Ebuka Ejiofor <ebuskelele@gmail.com>
2022-08-14 04:45:07 +0100

    Add animations, smooth scrolling and youtube video

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,77 @@
-// $("document").ready(function () {
-// Back to top
-// 	var backTop = $(".back-to-top");
-
-// 	$(window).scroll(function () {
-// 		if ($(document).scrollTop() > 400) {
-// 			backTop.css("visibility", "visible");
-// 		} else if ($(document).scrollTop() < 400) {
-// 			backTop.css("visibility", "hidden");
-// 		}
-// 	});
-
-// 	backTop.click(function () {
-// 		$("html").animate(
-// 			{
-// 				scrollTop: 0,
-// 			},
-// 			1000
-// 		);
-// 		return false;
-// 	});
-// });
+const backTop = document.querySelector(".back-to-top");
+const navbar = document.querySelector(".navbar");
 const hamburgerMenu = document.querySelector(".hamburger_menu");
+const navLinksContainer = document.querySelector(".links-container");
 const navLinks = document.querySelector(".links");
+const scrollLinks = document.querySelectorAll(".scroll-link");
+
+// Background Music
+// let audio = new Audio("/assets/sound/pangolin-mas-1.wav");
 
 hamburgerMenu.addEventListener("click", () => {
 	hamburgerMenu.classList.toggle("change");
-	navLinks.classList.toggle("show-links");
+
+	let containerHeight = navLinksContainer.getBoundingClientRect().height;
+	let linksHeight = navLinks.getBoundingClientRect().height;
+
+	if (containerHeight === 0) {
+		navLinksContainer.style.height = `${linksHeight}px`;
+	} else {
+		navLinksContainer.style.height = "0px";
+	}
 });
 
-// new WOW().init();
+// Scroll to top
+backTop.addEventListener("click", () => {
+	window.scrollTo({ left: 0, top: 0 });
+});
 
+scrollLinks.forEach((link) => {
+	link.addEventListener("click", (e) => {
+		e.preventDefault();
+		let id = e.target.getAttribute("href").slice(1);
+		const element = document.getElementById(id);
+
+		// Calculating heights
+		let navbarHeight = navbar.getBoundingClientRect().height;
+		const containerHeight =
+			navLinksContainer.getBoundingClientRect().height;
+		const fixedNav = navbar.classList.contains("fixed-navbar");
+
+		let position = element.offsetTop - navbarHeight;
+
+		if (!fixedNav) {
+			position -= navbarHeight;
+		}
+
+		if (navbarHeight > 104) {
+			position += containerHeight;
+		}
+
+		window, scrollTo({ left: 0, top: position });
+		hamburgerMenu.click();
+		if ((hamburgerMenu.style.display = "none")) {
+			hamburgerMenu.classList.remove("change");
+		}
+	});
+});
+
+// Fixed navbar and back-to-top button
+window.addEventListener("scroll", () => {
+	const scrollHeight = window.pageYOffset;
+	let navbarHeight = navbar.getBoundingClientRect().height;
+
+	if (scrollHeight > navbarHeight) {
+		navbar.classList.add("fixed-navbar");
+	} else {
+		navbar.classList.remove("fixed-navbar");
+	}
+
+	if (scrollHeight > 500) {
+		backTop.style.visibility = "visible";
+	} else {
+		backTop.style.visibility = "hidden";
+	}
+});
+
+new WOW().init();
